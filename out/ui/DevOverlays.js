46a6fd1364572a7353e3485ee50a3f5e61affdd8
Dae <136838382+WesHacixo@users.noreply.github.com>
2025-06-13 17:17:59 -0700

    Update .gitignore and remove large files

--- out/ui/DevOverlays.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const DevOverlays = () => ((0, jsx_runtime_1.jsx)("div", { style: { padding: 16, background: '#333', color: '#fff' }, children: "Dev Overlays (stub)" }));
-exports.default = DevOverlays;
-//# sourceMappingURL=DevOverlays.js.map
